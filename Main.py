eed9387b8057f0cdec3384c2cdab40e91e43f871
Hugop <hugo.petitprez@hotmail.com>
2020-06-24 16:24:47 +0200

    save damier

--- Main.py
+++ Main.py
@@ -3,4 +3,18 @@ from gui import Gui
 interface = Gui()
 interface.start()
 
-interface.updateCell(10,10,0)
+x = input("Largeur ?")
+y = input("Longeur ?")
+
+damier = Damier(x,y)
+
+nbgen = input("nb gen ?")
+if ...
+
+for i in range(nbgen):
+    damier.update()
+    interface.updateGrid(damier.grid)
+
+save = input("Sauvegarde ? (0/1)")
+if ....
+
