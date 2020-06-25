1626ba93e58a0623d11f6b28ced14a1953bfbea6
Hugop <hugo.petitprez@hotmail.com>
2020-06-25 19:39:00 +0200

    ACCART PETITPREZ JDLV

--- Main.py
+++ Main.py
@@ -6,15 +6,19 @@ interface.start()
 x = input("Largeur ?")
 y = input("Longeur ?")
 
-damier = Damier(x,y)
 
 nbgen = input("nb gen ?")
-if ...
+
 
 for i in range(nbgen):
     damier.update()
     interface.updateGrid(damier.grid)
 
-save = input("Sauvegarde ? (0/1)")
-if ....
+
+    save = input("Sauvegarde ? (0/1)")
+
+
+
+
+
 
